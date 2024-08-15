5f8469b899ed468850c3b2dd4540940adcf65fcc
nattumar <nattumar3@gmail.com>
2024-08-15 01:44:57 -0400

    final commit

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5001/dev-eb807/us-central1/app"
-   
+    //baseURL: "http://127.0.0.1:5001/dev-eb807/us-central1/app"
+   baseURL:"https://app-hugo6yrghq-uc.a.run.app"
 });
 
 export {axiosInstance};
