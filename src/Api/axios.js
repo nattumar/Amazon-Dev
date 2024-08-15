bcd926ba3baa857320ed3e917ad082bccbbfdf10
nattumar <nattumar3@gmail.com>
2024-08-15 13:08:03 -0400

    final axios update

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    //baseURL: "http://127.0.0.1:5001/dev-eb807/us-central1/app"
+
    baseURL:"https://app-hugo6yrghq-uc.a.run.app"
 });
 
