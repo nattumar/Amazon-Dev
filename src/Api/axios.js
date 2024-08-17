5ef8d5545b14ca862d8d2af9519fdca8bfa0818e
nattumar <nattumar3@gmail.com>
2024-08-16 21:15:39 -0400

    updated axios and removed build folder

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-
-   
-   baseURL:"https://app-hugo6yrghq-uc.a.run.app"
+baseURL:"https://app-hugo6yrghq-uc.a.run.app"
 });
 
 export {axiosInstance};
